f940a78b9d17da85407ffd88284c8d0f698c4894
alpinoblack <alpinoblack@gmail.com>
2024-12-21 01:56:48 +0200

    now can rotate all active images

--- scripts/image-rotate.js
+++ scripts/image-rotate.js
@@ -2,7 +2,7 @@
 let wheelPressCounter = 0;
 
 function pressingScrollButtonOnImage() {
-    const currentActiveImage = document.querySelector('div.ux-image-carousel-item,image-treatment,active,image');
+    const currentActiveImage = document.querySelector('div.ux-image-carousel-item.image-treatment.active.image');
     wheelPressCounter = (wheelPressCounter + 1) % 4;
     currentActiveImage.style.transform = `rotate(${90 * wheelPressCounter}deg)`;
 }
